fda04cb66f4f40bc39907f39590bc1478be82642
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-14 17:41:33 -0400

    Tired

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const session = require('express-session')
+
 const server = express()
+
+server.use(
+    session({
+        name: 'nonsession',
+        secret: 'Cheese is optional in a quesadilla',
+        cookie:{
+            maxAge: 1*24*60*60*1000,
+            secure: true
+        },
+        httpOnly: true,
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+
+
+
+
 
 const port = process.env.PORT || 4004;
 
