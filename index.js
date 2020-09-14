be15529a39b6851be7822883a065bf9591a6c874
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-14 18:02:25 -0400

    I think that's stretch, except I didn't make helpers and helpers would make it all work

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const session = require('express-session')
+const db = require('./knexconfig')
 
 const server = express()
 server.use(express.json())
@@ -18,6 +19,37 @@ server.use(
         saveUninitialized: false,
     })
 )
+server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+    const credentials = req.body;
+
+// find the user in the database by it's username then
+if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+  return res.status(401).json({ error: 'Incorrect credentials' });
+}
+  
+    users.findBy({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          // we will return 401 if the password or username are invalid
+          // we don't want to let attackers know when they have a good username
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+  server.post('/api/register', (req, res)=>{
+      const credentials = req.body;
+      const hash = bcrypt.hashSync(credentials.password, 14);
+      credentials.password = hash;
+  })
 
   server.get('/api/users', protected, (req, res) => {
     db('users') 
@@ -36,25 +68,7 @@ server.get('/api/logout', (req, res) =>{
         })
     }
 })
-server.post('/api/login', (req, res) => {
-    let { username, password } = req.body;
-  
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        // check that passwords match
-        if (user && bcrypt.compareSync(password, user.password)) {
-          res.status(200).json({ message: `Welcome ${user.username}!` });
-        } else {
-          // we will return 401 if the password or username are invalid
-          // we don't want to let attackers know when they have a good username
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  }); 
+ 
 function protected(req, res, next) {
     if (req.session && req.session.userId) {
       next();
