73a600ac81cd0b2125cb68cd3329389f987ca191
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-14 17:55:19 -0400

    Honestly, I just don't want to make the dbhelpers rn

--- migrations/20200914164110_users.js
+++ migrations/20200914164110_users.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schema
+    .createTable('users', tbl=>{
+        tbl.increment()
+        tbl.string('username')
+        tbl.string('password')
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+  return knex.schema
+  .dropTableIfExists('users')
 };
